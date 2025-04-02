d7e2738293f7688d4072ca7de9341fceb096c564
unknown <gaviriayeferson2@gmail.com>
2025-04-02 15:07:10 +0200

    first commit config

---
+++ src/main/java/com/f5/buzon_inteligente_BE/BuzonInteligenteBeApplication.java
@@ -0,0 +1,13 @@
+package com.f5.buzon_inteligente_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuzonInteligenteBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuzonInteligenteBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/f5/buzon_inteligente_BE/BuzonInteligenteBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f5.buzon_inteligente_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuzonInteligenteBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
