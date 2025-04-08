5ce4f77a0f03ebf3dcc403a4fc8fb249c3945821
unknown <gaviriayeferson2@gmail.com>
2025-04-08 12:18:26 +0200

    feat: Implement generateJwtToken method in JwtUtils to generate JWT with username, role, and dni

--- src/main/java/com/f5/buzon_inteligente_BE/security/JwtUtils.java
+++ src/main/java/com/f5/buzon_inteligente_BE/security/JwtUtils.java
@@ -1,5 +1,32 @@
 package com.f5.buzon_inteligente_BE.security;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
 public class JwtUtils {
 
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private long jwtExpirationMs;
+
+public String generateJwtToken (String username, String role, String dni)
+
+    {
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("role", role)
+                .claim("dni", dni)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
 }
