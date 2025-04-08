87b25932d2ac662615037c166cce6c1e4a5022b6
JoseRD149 <joseromerodiaz89@gmail.com>
2025-04-08 15:01:42 +0200

    add user model

--- src/main/java/com/f5/buzon_inteligente_BE/model/User.java
+++ src/main/java/com/f5/buzon_inteligente_BE/model/User.java
@@ -1,18 +1,37 @@
 package com.f5.buzon_inteligente_BE.model;
 
-public class User {
- 
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+
+    @Id
+    @GenertatedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
     public Long userId;
+
+    @Column(name = "user_DNI", nullable = false, length = 50)
     public String userDni;
+
     public String userName;
+
+    @Column(name = "user_mail", nullable = false, length = 50)
     public String userEmail;
+
+    @Column(name = "user_password", nullable = false, length = 50)
     public String userPasword;
 
+    @Column(name = "credential_id", nullable = false)
     public Long credentialId;
+
+    @Column(name = "locker_id", nullable = false)
+    public Long lockerId;
    
-  public Long lockerId;
-    
-       public User(Long userId, String userDni, String userName, String userEmail, String userPasword, Long credentialId,
+
+    public User(Long userId, String userDni, String userName, String userEmail, String userPasword, Long credentialId,
             Long lockerId) {
         this.userId = userId;
         this.userDni = userDni;
@@ -22,26 +41,32 @@ public class User {
         this.credentialId = credentialId;
         this.lockerId = lockerId;
     }
-    
-    public Long getUserId(){
+
+    public Long getUserId() {
         return userId;
     }
-    public String getUserDni(){
+
+    public String getUserDni() {
         return userDni;
     }
-    public String getUserName(){
+
+    public String getUserName() {
         return userName;
     }
-    public String getUserEmail(){
+
+    public String getUserEmail() {
         return userEmail;
     }
-    public String getUserPasword(){
+
+    public String getUserPasword() {
         return userPasword;
     }
-  public Long getCredentialId() {
+
+    public Long getCredentialId() {
         return credentialId;
     }
-public Long getLockerId() {
+
+    public Long getLockerId() {
         return lockerId;
     }
 
