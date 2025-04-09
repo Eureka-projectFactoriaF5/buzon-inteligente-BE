e221b847350fffaed47f251fded1afcf5732a5bb
Algama17 <albertogarciamallada@gmail.com>
2025-04-09 11:06:28 +0200

    Update UserRepository.java

--- src/main/java/com/f5/buzon_inteligente_BE/model/UserRepository.java
+++ src/main/java/com/f5/buzon_inteligente_BE/model/UserRepository.java
@@ -9,5 +9,4 @@ public interface UserRepository extends JpaRepository<User, Long>{
     Optional<User> findById(Long id);
     Optional<User> findByEmail(String email);
     Optional<User> findByUsername(String username);
-    User save(User user);
 }
