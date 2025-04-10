3308c4c523c7d2faa3caa57319c4222016646965
lannyRivero <lannyrivero288@gmail.com>
2025-04-10 12:17:37 +0200

    test: set up JwtAuthenticationFilterTest with required mocks and SecurityContext cleanup

---
+++ src/test/java/com/f5/buzon_inteligente_BE/security/JwtAuthenticationFilterTest.java
@@ -0,0 +1,48 @@
+package com.f5.buzon_inteligente_BE.security;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@DisplayName("JwtAuthenticationFilterTest")
+public class JwtAuthenticationFilterTest {
+    @Mock
+    private JwtUtils jwtUtils;
+
+    @Mock
+    private UserDetailsService userDetailsService;
+
+    @Mock
+    private HttpServletRequest request;
+
+    @Mock
+    private HttpServletResponse response;
+
+    @Mock
+    private FilterChain filterChain;
+
+    @Mock
+    private UserDetails userdetails;
+
+    @InjectMocks
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        SecurityContextHolder.clearContext();        
+    }
+
+    
+
+}
