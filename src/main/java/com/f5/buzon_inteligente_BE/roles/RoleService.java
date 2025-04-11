e48c8b34385d494d4e622ce9aaebc3344f015fd2
Algama17 <albertogarciamallada@gmail.com>
2025-04-11 12:03:28 +0200

    fix @service and exception add method findByRoleName included

--- src/main/java/com/f5/buzon_inteligente_BE/roles/RoleService.java
+++ src/main/java/com/f5/buzon_inteligente_BE/roles/RoleService.java
@@ -1,8 +1,9 @@
 package com.f5.buzon_inteligente_BE.roles;
 import java.util.HashSet;
 import java.util.Set;
+import org.springframework.stereotype.Service;
 
-
+@Service
 public class RoleService {
 
     private final RoleRepository repository;
@@ -12,12 +13,16 @@ public class RoleService {
     }
 
     public Role getById(Long id) {
-        Role role = repository.findById(id).orElseThrow();
+        Role role = repository.findById(id).orElseThrow(() -> new IllegalArgumentException("Role not found with ID: " + id));
         return role;
     }
 
+    private Role findByRoleName(String string) {
+        throw new UnsupportedOperationException("Unimplemented method 'findByRoleName'");
+    }
+
     public Set<Role> assignDefaultRole() {
-        Role defaultRole = this.getById(1L);
+        Role defaultRole = this.findByRoleName("USER");
 
         Set<Role> roles = new HashSet<>();
         roles.add(defaultRole);
