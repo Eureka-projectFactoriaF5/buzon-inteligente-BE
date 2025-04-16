85b3ea065b0f4ebdeb1bee66bb79f769c04532e2
Akalchi <alejandrafdez1699@gmail.com>
2025-04-16 10:54:39 +0200

    fix: add missing getter for lockers list in LockerStatus class

--- src/main/java/com/f5/buzon_inteligente_BE/locker/LockerStatus.java
+++ src/main/java/com/f5/buzon_inteligente_BE/locker/LockerStatus.java
@@ -38,6 +38,10 @@ public class LockerStatus implements Serializable {
         this.lockerStatusName = lockerStatusName;
     }
 
+    public List<Locker> getLockers() {
+        return lockers;
+    }
+
     public void setLockers(List<Locker> lockers) {
         this.lockers = lockers;
     }
