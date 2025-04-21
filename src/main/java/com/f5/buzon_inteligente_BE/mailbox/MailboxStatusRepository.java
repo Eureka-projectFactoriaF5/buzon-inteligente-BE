0a3fc9d2a41f43659a9cda4ac1b4d5825850f622
juancastro000 <cdyutub@gmail.com>
2025-04-21 12:00:24 +0200

    feat: add repositories for mailbox status and mailbox size

---
+++ src/main/java/com/f5/buzon_inteligente_BE/mailbox/MailboxStatusRepository.java
@@ -0,0 +1,9 @@
+package com.f5.buzon_inteligente_BE.mailbox;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MailboxStatusRepository extends JpaRepository<MailboxStatus, Long> {
+    Optional<MailboxStatus> findByStatusName(String mailboxStatusName);
+}
