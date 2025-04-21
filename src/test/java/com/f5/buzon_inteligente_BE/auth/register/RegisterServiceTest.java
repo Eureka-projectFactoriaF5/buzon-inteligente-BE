e9f8ea47008360278f778c4668bc8200db24a15d
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-04-21 16:12:28 +0200

    RegisterExceptions tested

--- src/test/java/com/f5/buzon_inteligente_BE/auth/register/RegisterServiceTest.java
+++ src/test/java/com/f5/buzon_inteligente_BE/auth/register/RegisterServiceTest.java
@@ -1,31 +1,33 @@
 package com.f5.buzon_inteligente_BE.auth.register;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.lang.reflect.Field;
 import java.util.Base64;
 import java.util.Map;
 import java.util.Optional;
 
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.f5.buzon_inteligente_BE.auth.register.RegisterExceptions.DniAlreadyExistsException;
+import com.f5.buzon_inteligente_BE.auth.register.RegisterExceptions.EmailAlreadyExistsException;
+import com.f5.buzon_inteligente_BE.auth.register.RegisterExceptions.RegisterException;
 import com.f5.buzon_inteligente_BE.profile.ProfileService;
 import com.f5.buzon_inteligente_BE.roles.Role;
 import com.f5.buzon_inteligente_BE.roles.RoleService;
-import com.f5.buzon_inteligente_BE.user.UserRepository;
 import com.f5.buzon_inteligente_BE.user.User;
-
+import com.f5.buzon_inteligente_BE.user.UserRepository;
 
 @ExtendWith(MockitoExtension.class)
 public class RegisterServiceTest {
@@ -44,25 +46,43 @@ public class RegisterServiceTest {
     @InjectMocks
     private RegisterService registerService;
 
+    private RegisterRequest registerRequest;
+    private Role testRole;
+    private String encodedPassword;
+
+    @BeforeEach
+    public void setup() {
+        
+        encodedPassword = Base64.getEncoder().encodeToString("Password".getBytes());
+        registerRequest = new RegisterRequest(
+            "DNI", 
+            "Name",
+            "Surname",
+            "test@example.com",
+            encodedPassword
+        );
+
+        testRole = new Role("ROLE_USER");
+
+    }
+
     @Test
     @DisplayName("Should RegisterUser success")
     void testRegisterUserSuccess() throws Exception, SecurityException {
-        RegisterRequest registerRequest =  new RegisterRequest("DNI", "Name", "Surname", "test@example.com",  Base64.getEncoder().encodeToString("Password".getBytes()));
         
         when(userRepository.findByUserEmail("test@example.com")).thenReturn(Optional.empty());
         when(userRepository.findByUserDni("DNI")).thenReturn(Optional.empty());
 
-        Role testRole = new Role("ROLE_USER");
         when(roleService.getDefaultRole()).thenReturn(testRole);
-        when(passwordEncoder.encode("Password")).thenReturn(Base64.getEncoder().encodeToString("Password".getBytes()));
+        when(passwordEncoder.encode("Password")).thenReturn(encodedPassword);
 
         User testUser = new User(
-            registerRequest.getUserDni(),
-            registerRequest.getUserName(),
-            registerRequest.getUserSurname(),
-            registerRequest.getUserEmail(),
-            registerRequest.getUserPassword(),
-            testRole);
+                registerRequest.getUserDni(),
+                registerRequest.getUserName(),
+                registerRequest.getUserSurname(),
+                registerRequest.getUserEmail(),
+                registerRequest.getUserPassword(),
+                testRole);
 
         Field field = User.class.getDeclaredField("userId");
         field.setAccessible(true);
@@ -71,11 +91,49 @@ public class RegisterServiceTest {
         when(userRepository.save(any(User.class))).thenReturn(testUser);
 
         Map<String, String> result = registerService.registerUser(registerRequest);
-        
+
         assertEquals("Success", result.get("message"));
         verify(userRepository).save(any(User.class));
         verify(passwordEncoder).encode("Password");
         verify(roleService).getDefaultRole();
     }
-    
+
+    @Test
+    @DisplayName("Should RegisterUser fail because email already exists")
+    void testRegisterUserFailEmailAlreadyExists() throws Exception, SecurityException {
+        when(userRepository.findByUserEmail("test@example.com")).thenReturn(Optional.of(new User()));
+
+        EmailAlreadyExistsException exception = assertThrows(EmailAlreadyExistsException.class,
+                () -> registerService.registerUser(registerRequest));
+
+        verify(userRepository).findByUserEmail("test@example.com");
+        assertEquals("El email ya está registrado", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Should RegisterUser fail because DNI already exists")
+    void testRegisterUserFailDniAlreadyExists() throws Exception, SecurityException {
+        when(userRepository.findByUserDni("DNI")).thenReturn(Optional.of(new User()));
+
+        DniAlreadyExistsException exception = assertThrows(DniAlreadyExistsException.class,
+                () -> registerService.registerUser(registerRequest));
+
+        verify(userRepository).findByUserDni("DNI");
+        assertEquals("El DNI ya está registrado", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Should throw exception when password cannot be decoder")
+    void testRegisterUserFailPasswordCannotBeDecoder() throws Exception, SecurityException {
+        registerRequest.setUserPassword("###Password###");
+
+        when(userRepository.findByUserEmail("test@example.com")).thenReturn(Optional.empty());
+        when(userRepository.findByUserDni("DNI")).thenReturn(Optional.empty());
+
+        RegisterException exception = assertThrows(RegisterException.class,
+                () -> registerService.registerUser(registerRequest));
+                
+        assertEquals("Error decoding password from Base64", exception.getMessage());
+    }
+        
 }
