0326b613920c5952d778915e4f90c84f56f596d1
lannyRivero <lannyrivero288@gmail.com>
2025-04-22 11:02:35 +0200

    test: enhance LockerTest with constructor validation

--- src/test/java/com/f5/buzon_inteligente_BE/locker/LockerTest.java
+++ src/test/java/com/f5/buzon_inteligente_BE/locker/LockerTest.java
@@ -1,7 +1,11 @@
 package com.f5.buzon_inteligente_BE.locker;
 
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 
 class LockerTest {
@@ -14,5 +18,13 @@ class LockerTest {
         lockerStatus = new LockerStatus();
         locker = new Locker("Calle 123", 48L, lockerStatus);
     } 
+
+    @Test
+    @DisplayName("Should constructor set address, timeLimit and lockerStatus correctly")
+    void testShouldConstructor() {
+        assertThat(locker.getAddress()).isEqualTo("Calle 123");
+        assertThat(locker.getTimeLimit()).isEqualTo(48L);
+        assertThat(locker.getLockerStatus()).isEqualTo(lockerStatus);
+    }
 
 }
