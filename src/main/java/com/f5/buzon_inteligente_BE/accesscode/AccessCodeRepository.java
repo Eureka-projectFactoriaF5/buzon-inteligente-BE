5e545c8d0e6242e476b5086336162ceeb316e81a
unknown <gaviriayeferson2@gmail.com>
2025-04-22 13:08:33 +0200

    feat: implement AccessCodeService with create and retrieve functionality for access codes

---
+++ src/main/java/com/f5/buzon_inteligente_BE/accesscode/AccessCodeRepository.java
@@ -0,0 +1,18 @@
+package com.f5.buzon_inteligente_BE.accesscode;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccessCodeRepository extends JpaRepository<AccessCode, Long> {
+
+    boolean existsByAccessCode(String accessCode);  
+
+    Optional<AccessCode> findByAccessCode(String accessCode);
+    
+    List<AccessCode> findAllByProfile_ProfileId(Long profileId);
+
+}
