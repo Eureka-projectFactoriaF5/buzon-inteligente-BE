fbbe8921f82f28fbdf6b1181c5a3de2eb2938d87
AnaBHernandez <[anahernandezfit@gmail.com]>
2025-04-22 12:59:56 +0200

    Feat: Endpoint created to obtain the access code

--- src/main/java/com/f5/buzon_inteligente_BE/accesscode/AccesCodeController.java
+++ src/main/java/com/f5/buzon_inteligente_BE/accesscode/AccesCodeController.java
@@ -1,7 +1,12 @@
 package com.f5.buzon_inteligente_BE.accesscode;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,15 +20,23 @@ public class AccesCodeController {
 
     AccessCodeService accessCodeService;
 
-    public AccessCodeController(AccessCodeService accessCodeService){
+    public AccessCodeController(AccessCodeService accessCodeService) {
         this.accessCodeService = accessCodeService;
     }
 
     @PostMapping
-    public ResponseEntity<AccessCodeResponseDTO> createAccessCode(AccessCodeRequestDTO accessCodeRequestDTO){
+    public ResponseEntity<AccessCodeResponseDTO> createAccessCode(AccessCodeRequestDTO accessCodeRequestDTO) {
         AccessCode createdAccessCode = accessCodeService.createAccessCode(accessCodeRequestDTO);
         return new ResponseEntity<>(AccessCodeResponseDTO.fromEntities(createdAccessCode),
-        HttpStatus.CREATED);
+                HttpStatus.CREATED);
     }
-    
+
+    @GetMapping("/profile/{profileId}")
+    public ResponseEntity<List<AccessCodeResponseDTO>> getAccessCodesByProfile(@PathVariable Long profileId) {
+        List<AccessCode> accessCodes = accessCodeService.getAccessCodesByProfileId(profileId);
+        List<AccessCodeResponseDTO> accessCodeDTOs = accessCodes.stream()
+                .map(AccessCodeResponseDTO::fromEntities)
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(accessCodeDTOs, HttpStatus.OK);
+    }
 }
